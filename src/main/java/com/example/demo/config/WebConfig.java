cbdfcb60fc14124e336d07a836f769d14142f3ed
iblug <1115jkb@gmail.com>
2023-12-01 11:38:50 +0900

    외부 이미지 보이기
    외부 이미지 보이기

---
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.example.demo.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer{
+	
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    	
+    	registry.addResourceHandler("/profile/**")
+    			.addResourceLocations("file:///profile/");
+    }
+}
+
